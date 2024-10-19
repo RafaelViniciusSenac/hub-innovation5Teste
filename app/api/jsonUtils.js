eb82a8165a3810223a33cc541311cfc4245ccd7c
Rafael Vinicius <42884321+rafavini@users.noreply.github.com>
2024-10-19 19:35:34 -0300

    Fix: correçao o problema read-only da vercel colocando o caminho da pasta /tmp

--- app/api/jsonUtils.js
+++ app/api/jsonUtils.js
@@ -2,12 +2,21 @@
 import fs from 'fs';
 import path from 'path';
 
-const filePath = path.join(process.cwd(), 'app', 'api', 'db', 'data.json');
+const isVercel = process.env.VERCEL === '1'; // Verifica se está em ambiente Vercel
+const filePath = isVercel
+    ? path.join('/tmp', 'data.json') // Usar /tmp no Vercel
+    : path.join(process.cwd(), 'app', 'api', 'db', 'data.json'); // Usar localmente
 
 export async function readJsonFile() {
     try {
-        const jsonData = fs.readFileSync(filePath, 'utf-8');
-        return JSON.parse(jsonData);
+        // Verifica se o arquivo existe antes de tentar ler
+        if (fs.existsSync(filePath)) {
+            const jsonData = fs.readFileSync(filePath, 'utf-8');
+            return JSON.parse(jsonData);
+        } else {
+            console.warn("File does not exist, returning empty array.");
+            return []; // Retorna um array vazio se o arquivo não existir
+        }
     } catch (error) {
         console.error("Error reading JSON file:", error);
         return null;
