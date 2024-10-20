bc68249c557b68cba0d3e956a7b540e762e7aa23
Rafael Vinicius <42884321+rafavini@users.noreply.github.com>
2024-10-19 21:04:55 -0300

    Fix: Correçao visual do mobile

--- app/(pages)/home/page.js
+++ app/(pages)/home/page.js
@@ -22,15 +22,15 @@ export default function Home() {
             alert(resposta.message)
         }
     }
-    
+
     useEffect(() => {
         GetUserData()
     }, []);
 
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto px-4 ">
             <div className="flex flex-col justify-center items-center ">
-                <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center">Bem-vindo</h1>
+                <h1 className="text-4xl font-bold text-gray-800 mb-4 text-center mt-10">Bem-vindo</h1>
             </div>
             <div>
                 <div className="flex justify-center items-center">
@@ -39,8 +39,8 @@ export default function Home() {
                 <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {userData.map((item, index) => (
                         <div key={index} className="bg-white shadow-lg rounded-lg p-6 border border-gray-200">
-                            <h2 className="text-2xl font-semibold text-gray-700 mb-2">{item.titulo}</h2>
-                            <p className="text-gray-600 mb-1">Usuário: {item.usuario}</p>
+                            <h2 className="text-2xl font-semibold text-gray-700 mb-2 text-center">{item.titulo}</h2>
+                            <p className="text-gray-600 mb-1 ">Usuário: {item.usuario}</p>
                             <p className="text-gray-600 mb-4">Senha: {item.senha}</p>
                             <div className="flex gap-2 justify-end">
                                 <button onClick={() => handleDelete(item.id)} className="bg-red-500 text-white px-4 py-2 rounded">
